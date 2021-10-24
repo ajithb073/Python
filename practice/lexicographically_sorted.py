10b9cb0252238e3f6d132e667d703dba0936a927
Ajith Babu <39835494+ajithb073@users.noreply.github.com>
2021-10-24 21:13:28 +0530

    Create  lexicographically_sorted.py

---
+++ practice/lexicographically_sorted.py
@@ -0,0 +1,41 @@
+# 24/11/2021
+
+# 10
+# fortran
+# java
+# perl
+# python
+# php
+# javascript
+# c
+# cpp
+# ruby
+# csharp
+
+
+
+def main():
+    all_len = int(input())  #10
+    # print(all_len)
+    all_words = []
+    for i in range(all_len):
+        words = input()
+        all_words.append(words)
+    all_words = sorted(all_words)   #lexicographically sorting 
+    # print(all_words)
+    for i in all_words:
+        print(i)
+        
+        
+# output        
+        
+# c
+# cpp
+# csharp
+# fortran
+# java
+# javascript
+# perl
+# php
+# python
+# ruby        
